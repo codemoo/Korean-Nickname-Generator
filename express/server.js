9ef85f5a8dce0c213ac6aab0c22944c0447b07c1
codemoo <hwanmooy@gmail.com>
2020-06-24 18:33:05 +0900

    Update server.js

--- express/server.js
+++ express/server.js
@@ -91,8 +91,8 @@ setInterval(function() {
     // just wrap your code in an async function that gets called immediately
     (async function main() {
         await doc.useServiceAccountAuth({
-            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
+            client_email: `${process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL}`,
+            private_key: `${process.env.GOOGLE_PRIVATE_KEY}`,
         });
         await doc.loadInfo(); // loads document properties and worksheets
         // console.log(doc.title);
@@ -120,8 +120,8 @@ setInterval(function() {
 
 (async function main() {
     await doc.useServiceAccountAuth({
-        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        client_email: `${process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL}`,
+        private_key: `${process.env.GOOGLE_PRIVATE_KEY}`,
     });
     await doc.loadInfo(); // loads document properties and worksheets
     // console.log(doc.title);
