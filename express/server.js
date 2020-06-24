af99b00000e63f8b864e36a721cf36cc5e925b86
codemoo <hwanmooy@gmail.com>
2020-06-24 17:44:06 +0900

    update

--- express/server.js
+++ express/server.js
@@ -4,9 +4,14 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+var words = [];
+
+const csv = require('csv-parser');
+const fs = require('fs');
+
 const router = express.Router();
 app.get('/', (request, response) => {
-    // console.log(words)
+    
     if (request.query.count === undefined || request.query.count === '') {
         var loop = 1
     } else {
@@ -38,38 +43,32 @@ app.get('/', (request, response) => {
     
 })
 
-
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
 
+// fs.createReadStream('./misc/words.csv')
+// .pipe(csv())
+// .on('data', (row) => {
+//     words.push(row);
+// })
+// .on('end', () => {
+//     console.log('CSV file successfully processed');
+//     console.log(words)    
+// });
+
 // 닉네임 생성 관련
-const csv = require('csv-parser');
-const fs = require('fs');
 var seedrandom = require('seedrandom');
 
 seedrandom('hello.', { global: true }); 
-
-let words = [];
-
-fs.createReadStream('./misc/words.csv')
-.pipe(csv())
-.on('data', (row) => {
-    words.push(row);
-})
-.on('end', () => {
-    console.log('CSV file successfully processed');
-
-    app.use(bodyParser.json());
-    app.use('/.netlify/functions/server', router);  // path must route to lambda
-    
-});
 
 function genWord(words) {
     var adj = words[Math.floor(Math.random() * words.length)]['adj'];
     var noun = words[Math.floor(Math.random() * words.length)]['noun'];
 
-    return result = adj + ' ' + noun;
+    return adj + ' ' + noun;
 }
 
 const { GoogleSpreadsheet } = require('google-spreadsheet');
@@ -86,7 +85,7 @@ const csvWriter = createCsvWriter({
     ]
 });
 
-var minutes = 5, the_interval = minutes * 60 * 1000;
+var minutes = 5, the_interval = minutes * 1 * 1000;
 setInterval(function() {
 
     // just wrap your code in an async function that gets called immediately
@@ -95,18 +94,17 @@ setInterval(function() {
             client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
             private_key: process.env.GOOGLE_PRIVATE_KEY,
         });
-
         await doc.loadInfo(); // loads document properties and worksheets
         // console.log(doc.title);
-
+    
         const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
         // console.log(sheet.title);
         // console.log(sheet.rowCount);
-
+    
         // read rows
         const rows = await sheet.getRows(); // can pass in { limit, offset }
-
-        let words = [];
+    
+        words = [];
         rows.forEach(row => {
             let _data = {
                 'id':row._rawData[0],
@@ -115,11 +113,34 @@ setInterval(function() {
             }    
             words.push(_data);
         });
-
-        csvWriter
-        .writeRecords(words)
-        .then(()=> console.log('The CSV file was written successfully'));
-
+    
     })();
 
 }, the_interval);
+
+(async function main() {
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+    });
+    await doc.loadInfo(); // loads document properties and worksheets
+    // console.log(doc.title);
+
+    const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+    // console.log(sheet.title);
+    // console.log(sheet.rowCount);
+
+    // read rows
+    const rows = await sheet.getRows(); // can pass in { limit, offset }
+
+    words = [];
+    rows.forEach(row => {
+        let _data = {
+            'id':row._rawData[0],
+            'adj':row._rawData[1],
+            'noun':row._rawData[2],
+        }    
+        words.push(_data);
+    });
+
+})();
